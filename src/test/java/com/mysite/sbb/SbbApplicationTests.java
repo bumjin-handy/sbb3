0b0369ca17faa4c3ee64deca74320f03e6211a5f
bumjin-handy <bumjin@handysoft.co.kr>
2024-01-11 21:52:18 +0900

    2-05 리포지터리로 데이터베이스 관리하기
    findBySubjectAndContent 와 findBySubjectLike 테스트

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -21,8 +21,17 @@ class SbbApplicationTests {
 	}
 
 	@Test
-	void testJpa() {
-		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+	void testJpaFindBySubjectAndContent() {
+		Question q = questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?",
+				"sbb에 대해 알고 싶습니다.");
 		assertEquals(1, q.getId());
+	}
+
+	@Test
+	void testJpaFindBySubjectLike() {
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 }
